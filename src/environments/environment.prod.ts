e8cc15fbe920816848227ec7c077b1e42c15ef35
Christopher <161456547+JurdaParem@users.noreply.github.com>
2025-12-07 14:50:06 +0200

    Updated git ignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // Add your production EmailJS IDs here
+  emailjs: {
+    serviceId: 'service_dn1rt99', // Often the same as development, but good practice to keep separate
+    templateId: 'template_agfljta', // Often the same as development
+    publicKey: 'v06tRebjQEO3eXs4x',
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  // Add your EmailJS IDs here for development/testing
+   emailjs: {
+    serviceId: 'service_dn1rt99', // Often the same as development, but good practice to keep separate
+    templateId: 'template_agfljta', // Often the same as development
+    publicKey: 'v06tRebjQEO3eXs4x',
+  }
+};
